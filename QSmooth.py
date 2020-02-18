9d28dfdcfec85d66dafa414b6ddfa95145f1bca9
DominikaDu <dominika.durovcikova@gmail.com>
2020-02-18 14:04:45 +0000

    changed smooth to include redshift as a parameter

--- QSmooth.py
+++ QSmooth.py
@@ -24,7 +24,7 @@ def running_median(datx,daty,bin_size=30,shuffle=5,Lya=False):
     # Creates a running median of data given by datx and daty.
     # bin_size = number of data points to calculate one median value from
     # shuffle = step size as the number of data points to shuffle over
-    # near_Lya = 
+    # Lya = boolean parameter to reduce the bin size and shuffle in the vicinity of Lya peak
 
     j = 0
     xvals = []
@@ -91,27 +91,15 @@ def running_median(datx,daty,bin_size=30,shuffle=5):
             break            
     return np.array(xvals),np.array(yvals)
 
-def smooth(filename,path,mask=None):
-	# Smooths an SDSS spec-PLATE-MJD-FIBER.fits file according to procedure outlined in Appendix B
-    # of Durovcikova et al. 2019 (https://arxiv.org/abs/1912.01050). In this process, a mask can be 
-    # used to reject some data from the spec file.
+def smooth(x,y,y_err,redshift,mask=None):
+	# Smooths raw input spectral data given by (x,y) and errors y_err according to procedure outlined in Appendix B
+    # of Durovcikova et al. 2019 (https://arxiv.org/abs/1912.01050) for a given value of calibration redshift.
+    # In this process, a mask can be used to reject some data from the spec file.
 
-	hdu_raw = pf.open(str(path)+str(filename))
     if len(mask)>0:
-	    hdu = hdu_raw[1].data[mask]
-    else:
-        hdu = hdu_raw[1].data
-    # Either calibrate to rest wavelengths based on redshift from a particular line, e.g. Mg-II
-    # https://data.sdss.org/datamodel/files/BOSS_SPECTRO_REDUX/RUN2D/PLATE4/RUN1D/spZline.html
-	# x = hdu['loglam'] - np.log10(1+hdu_raw[3].data['LINEZ'][5])
-    # Or calibrate to rest wavelengths based on the redshift from the SDSS pipelines.
-    x = hdu['loglam'] - np.log10(1+hdu_raw[2].data['Z'])
-	y = hdu['flux']
-	err = hdu['ivar']
-
-    #####
-    # Move all of the above to an outside function and have only (x,y,redshift,mask) as input
-    #####
+	    x = x[mask]
+        y = y[mask]
+        y_err = y_err[mask]
 	
 	# 1. Compute upper envelope:
 	[x_bor,y_bor] = running_median(x,y,bin_size=50,shuffle=10)
@@ -125,9 +113,9 @@ def smooth(filename,path,mask=None):
 	
 	# 3. Apply RANSAC to detect outlying pixels (absorption features) and mask them out.
 	# Note: we weigh the raw data points according to their errors.
-	mad = np.average(np.abs(np.median(linear_y)-linear_y),weights=np.divide(err,np.sum(err)))
+	mad = np.average(np.abs(np.median(linear_y)-linear_y),weights=np.divide(y_err,np.sum(y_err)))
 	ransac = linear_model.RANSACRegressor(random_state=0,loss='absolute_loss',residual_threshold=2.0*mad)
-	ransac.fit(x.reshape(len(x),1), linear_y,sample_weight=np.abs(err))
+	ransac.fit(x.reshape(len(x),1), linear_y,sample_weight=np.abs(y_err))
 	inlier_mask = ransac.inlier_mask_
 	outlier_mask = np.logical_not(inlier_mask)
 	
